5f5a9eb51c5dcc9497befe89dd62663db73d1fa1
Kavita Chaudhry <kavita.chaudhry@intersection.com>
2020-03-22 23:11:28 -0400

    header works

--- client/app/src/app/components/App.js
+++ client/app/src/app/components/App.js
@@ -6,23 +6,10 @@ import '../../styles/app.css';
 import hamburger from '../../images/icons/hamburger.svg';
 import bgImage from '../../images/kavita_background.jpg';
 
-
 const FlexDiv = styled.div`
- display:flex;
- justify-content: flex-end;
- 
-`;
-
-// const ParentHeaderDiv = styled.div`
-// display: flex;
-// flex-direction: column;
-// grid-row:1;
-// `;
-
-
-const BackgroundImg = styled.img`
-  width: 100%;
-      -webkit-font-smoothing: antialiased;
+  display: flex;
+  justify-content: flex-end;
+  background-color: white;
 `;
 
 export class App extends React.PureComponent {
@@ -40,24 +27,28 @@ export class App extends React.PureComponent {
       <div className={'container'}>
         <div className={'parent-header-div'}>
           <FlexDiv>
-            <div className={'top-caption-header'}>“LIFE IS EITHER A DARING ADVENTURE OR NOTHING AT ALL.” - HELEN KELLER</div>
+            <div className={'top-caption-header'}>
+              “LIFE IS EITHER A DARING ADVENTURE OR NOTHING AT ALL.” - HELEN
+              KELLER
+            </div>
           </FlexDiv>
           <div className={'header-div'}>
             <div className={'test'}>
               <div className={'navigation-header'}>
-                KAVITA CHAUDHRY<img src={hamburger}/></div>
+                KAVITA CHAUDHRY
+                <img src={hamburger} />
+              </div>
             </div>
-
           </div>
         </div>
-      </div>);
+      </div>
+    );
   }
 }
 
-const mapDispatchToProps = dispatch =>
-  ({
-    hello: () => dispatch(sayHello())
-  });
+const mapDispatchToProps = dispatch => ({
+  hello: () => dispatch(sayHello())
+});
 
 const mapStateToProps = state => ({
   helloState: state.hello
